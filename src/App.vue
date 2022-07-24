e761e4913252a4a9f420d3538e6de087ce878c88
Vegard Løkås <vegardlokas@hotmail.com>
2022-07-24 17:25:01 +0200

    done with all tasks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-</template>
+<script setup lang="ts">
+import {reactive} from 'vue'
+import Counter from './components/Counter.vue'
 
-<script lang="ts">
+interface AppInfo {
+	name: string;
+	slogan: string;
+}
+
+const appInfo:AppInfo = reactive({
+	name: 'vegard',
+	slogan: 'yolo!'
+})
+
+
 </script>
 
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+  <h1>{{ appInfo.name }}</h1>
+  <h2>{{appInfo.slogan}}</h2>
+  <Counter
+	:limit="10"
+  ></Counter>
+  
+  </div>
+</template>
---
+++ src/components/ControlBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const emit = defineEmits<{
+     ( e: 'add-count', num: number): void;
+     ( e: 'reset-count'): void;
+}>()
+
+</script>
+
+<template>
+  <p>
+    <button @click="emit('add-count', 1)">Add</button>
+    <button @click="emit('reset-count')">Reset</button>
+  </p>
+</template>
